fa91f0cd8490f326ea94a8cad75c75c399afff41
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-05-30 13:46:57 -0700

    ngFor loop

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'no servers created';
   serverName = '';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() {
     setTimeout(()=> {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'The server was created. Name is ' + this.serverName;
   }
 
